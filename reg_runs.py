d98d653a44073b2cc3740559b01003550b7ff08b
Robert <54269637+R-udren@users.noreply.github.com>
2023-12-26 16:41:23 +0200

    Added type annotation and Exceptions with logger

--- reg_runs.py
+++ reg_runs.py
@@ -2,44 +2,54 @@ import winreg
 import json
 import logging
 from pprint import pprint as pp
+from typing import Dict, List, Union, Tuple
+
+logger = logging.getLogger(__name__)
 
 
-def load_reg_paths(json_path) -> dict:
+def load_reg_paths(json_path: str) -> Dict[str, List[str]]:
     try:
         with open(json_path, 'r') as f:
             reg_paths = json.load(f)
     except FileNotFoundError:
-        logging.error(f"{f.name} not found")
+        logger.error(f"{json_path} not found")
+        reg_paths = {}
     return reg_paths
 
 
-def get_reg_values(key, subkey):
+def get_reg_values(key: int, subkey: str) -> Dict[str, Union[str, int]]:
     reg_values = {}
-    with winreg.OpenKey(key, subkey) as reg_key:
-        for i in range(winreg.QueryInfoKey(reg_key)[1]):
-            name, value, _ = winreg.EnumValue(reg_key, i)
-            reg_values[name] = value
-
+    try:
+        with winreg.OpenKey(key, subkey) as reg_key:
+            for i in range(winreg.QueryInfoKey(reg_key)[1]):
+                name, value, _ = winreg.EnumValue(reg_key, i)
+                reg_values[name] = value
+    except PermissionError as pe:
+        logger.error(f"Permission error: Unable to open registry key {subkey}: {pe}")
+    except Exception as e:
+        logger.error(f"Error reading registry key {subkey}: {e}")
     return reg_values
 
 
-def extract_hive_key(registry_path):
-    hive = registry_path.split("\\")[0]
-    subkey = "\\".join(registry_path.split("\\")[1:])
+def extract_hive_key(registry_path: str) -> Tuple[int, str]:
+    hive, subkey = registry_path.split("\\", 1)
     try:
         key = getattr(winreg, hive)
     except AttributeError:
-        logging.error(f"{hive} is not a valid hive")
+        logger.error(f"{hive} is not a valid registry hive")
         key = None
     return key, subkey
 
 
 def main():
     paths = load_reg_paths("paths.json")
-    registry_paths = paths["registry_paths"]
+    registry_paths = paths.get("registry_paths", [])
+
     for registry_path in registry_paths:
-        reg_values = get_reg_values(*extract_hive_key(registry_path))
-        pp(reg_values)
+        key, subkey = extract_hive_key(registry_path)
+        if key is not None:
+            reg_values = get_reg_values(key, subkey)
+            pp(reg_values)
 
 
 if __name__ == "__main__":
